4d16747e277d34180f777312fad09d12b966b896
Juan Fung <juanfung@gmail.com>
2019-01-10 07:44:05 -0500

    Update package setup prepLoad.R to use here for better file-path management

--- Prep/prepLoad.R
+++ Prep/prepLoad.R
@@ -4,28 +4,31 @@
 
 ## ----------------------------------------------------------------------------------- #
 
-## set path; change as necessary
-home.dir <- ".."
+## using rprojroot via here for better directory management
+library("here")
 
 ## Path to data, change as necessary
 
-path.to.data <- paste0(home.dir,"/Data/")
-path.to.custom <- paste0(home.dir,"/Functions/")
+path.to.data <- here("Data")
+path.to.custom <- here("Functions")
 
 ## ----------------------------------------------------------------------------------- #
 
 ## Packages #
 
-library(data.table)
-library(plyr)
-library(dplyr)
+library("data.table")
+## TODO:
+## replace usage of plyr with dplyr or appropriate substitute
+library("plyr")
+library("dplyr")
 
-library(rgdal)
-library(rgeos)
-library(maptools)
+library("rgdal")
+library("rgeos")
+library("sf")
+library("maptools")
 
 ## Functions #
 
-source(paste0(path.to.custom,"prepFunctions.R"))
+source(file.path(path.to.custom, "prepFunctions.R"))
 
 ## ----------------------------------------------------------------------------------- #
